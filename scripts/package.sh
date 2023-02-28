1ba507b3f8a5d8466bc0257321c13799ba8c491d
Daishan Peng <daishan@acorn.io>
2023-02-27 23:30:22 -0700

    More chart fix
    Signed-off-by: Daishan Peng <daishan@acorn.io>

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,6 +3,6 @@ set -e -x
 
 cd docs/
 helm package ../
+helm package ../charts/aws-cloud-controller-manager
 cd ..
-helm repo index --url https://acorn-io.github.io/hub-system-charts/ .
-mv index.yaml docs/
+helm repo index --url https://acorn-io.github.io/hub-system-charts/ ./docs
