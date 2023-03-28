51ab7c9066ee9e1ee2217e9a6843d90762a3c7ac
Bill Maxwell <cloudnautique@users.noreply.github.com>
2023-03-27 20:35:13 -0700

    Add linkerd chart with patch for injector
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,5 +4,9 @@ set -e -x
 cd docs/
 helm package ../
 helm package ../charts/aws-cloud-controller-manager
+pushd ../charts/linkerd/linkerd-control-plane
+helm dep up
+popd
+helm package ../charts/linkerd/linkerd-control-plane
 cd ..
 helm repo index --url https://acorn-io.github.io/hub-system-charts/ ./docs
